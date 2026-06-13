3465c459151bbe68ee96aa9e0ba99e40a7fd4407
Syam Prasad <syamofficial1212@gmail.com>
2026-06-13 16:21:40 +0530

    Create Maximum-product-subarray-in-a-array.java

---
+++ Subarrays/Maximum-product-subarray-in-a-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    int maxProduct(int[] arr) {
+         int n=arr.length;
+        int pre=1,suf=1;
+        int ans=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            if(pre==0) pre= 1;
+            if(suf==0) suf=1;
+
+            pre*=arr[i];
+            suf*=arr[n-i-1];
+            ans=Math.max(ans,Math.max(suf,pre));
+        }
+        return ans;
+    }
+}
