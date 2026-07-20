8d9eef75d13da9b1b1e04bbb5bb044e2eb99242a
Syam Prasad <syamofficial1212@gmail.com>
2026-07-20 20:36:55 +0530

    Implement length calculation for linked list

---
+++ Linked-List/Basic/Length-of-a-linked-list.java
@@ -0,0 +1,23 @@
+/* Structure of linked list Node
+class Node{
+    int data;
+    Node next;
+
+    Node(int a){
+        data = a;
+        next = null;
+    }
+}
+*/
+class Solution {
+    public int getCount(Node head) {
+        // code here
+        Node temp=head;
+        int cnt=0;
+        while(temp!=null){
+            cnt++;
+            temp=temp.next;
+        }
+        return cnt;
+    }
+}
