67f4716dda6969859d210de734dcae540ecfeb3b
Syam Prasad <syamofficial1212@gmail.com>
2026-07-22 09:34:17 +0530

    Add method to traverse and print linked list
    Implement printList method to traverse and print linked list elements.

---
+++ Linked-List/Basic/Traversing-A-Linked-List.java
@@ -0,0 +1,24 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int val) {
+        data = val;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    void printList(Node head) {
+        // code here
+        if(head==null) System.out.print(" ");
+        Node temp=head;
+        
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+    }
+}
