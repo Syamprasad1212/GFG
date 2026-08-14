78882c948c384f5dd7677f6367fb8d6df1371b47
Syam Prasad <syamofficial1212@gmail.com>
2026-08-14 11:35:30 +0530

    Add largestPrimeFactor method to find largest prime factor

---
+++ MATH/largest-Prime-Factors.java
@@ -0,0 +1,14 @@
+class Solution {
+    static int largestPrimeFactor(int n) {
+            int res=0;
+         for(int i=2;i*i<=n;i++){
+            while(n%i==0){
+                res=i;
+                n=n/i;
+            }
+           }
+           if(n>1) res=n;
+           
+           return res;
+    }
+}
